f293e5f5e6f7105f266b697cc3ad4072831fc584
Carl0sPineda <carlos7712p@gmail.com>
2024-04-22 17:18:01 -0600

    feat: sweetalert2 added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
 import esLocale from "@fullcalendar/core/locales/es";
 import AddEventModal from "../components/AddEventModal";
+import Swal from "sweetalert2";
 import plus from "../assets/plus.svg";
 
 function Home() {
@@ -15,12 +16,20 @@ function Home() {
 
   const handleEventClick = (info: EventClickArg) => {
     const eventId = info.event.id;
-    const shouldDelete = window.confirm(
-      "Are you sure you want to delete this event?"
-    );
-    if (shouldDelete) {
-      onDelete(eventId);
-    }
+    Swal.fire({
+      title: "Estas seguro?",
+      text: "No puedes revertir esta acción luego!",
+      showCancelButton: true,
+      confirmButtonColor: "red",
+      cancelButtonColor: "black",
+      cancelButtonText: "No, cancelar",
+      confirmButtonText: "Sí, continuar",
+      background: "#00000",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDelete(eventId);
+      }
+    });
   };
 
   const onDelete = async (id: string) => {
